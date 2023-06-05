39295c7e936e3bae10782231d8093e5cf90c05f1
nowshin51 <sanjina15-13151@diu.edu.bd>
2023-06-05 13:17:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Header></Header> */}
+      <Countries></Countries>
+      {/* <LoadCountries></LoadCountries> */}
     </div>
   );
 }
+
+// function LoadCountries(){
+//   const [countries, setCountries] = useState([]);
+//   useEffect( () => {
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res => res.json())
+//     .then(data => setCountries(data))
+//   }, [])
+//   return(
+//     <div>
+//       <h1>Visiting countries of the world!!</h1>
+//       <h3>Available countries : {countries.length}</h3>
+//     </div>
+//   )
+// }
 
 export default App;
